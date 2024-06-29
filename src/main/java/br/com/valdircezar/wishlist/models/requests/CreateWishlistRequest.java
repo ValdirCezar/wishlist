ea607980d50aa23e32b090c34114f198e4f55b3e
= <=>
2024-06-29 08:36:43 -0300

    feat: add productsId example on create a wishlist

--- src/main/java/br/com/valdircezar/wishlist/models/requests/CreateWishlistRequest.java
+++ src/main/java/br/com/valdircezar/wishlist/models/requests/CreateWishlistRequest.java
@@ -15,7 +15,9 @@ public record CreateWishlistRequest(
 
         @NotBlank(message = "Field userId is required")
         @Size(min = 14, max = 30, message = "Field userId must be between 14 and 30 characters")
-        @Schema(description = "User ID", example = "64bb3bbe319d2b6e43ba05dd")
+        @Schema(description = "User ID", example = "606b3efb74e106091aae50d8")
         String userId,
+
+        @Schema(description = "List of products ID", example = "[\"997f2e1a9f5cf6e2ca4beae4\",\"333abc510d2ef9143eefa7f1\"]")
         List<String> productsId
 ) { }
