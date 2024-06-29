1024cb6dd0cce11705f87d3de471ce6204e046d9
= <=>
2024-06-29 07:57:06 -0300

    feat: add exception handler classes

--- src/main/java/br/com/valdircezar/wishlist/controllers/WishlistController.java
+++ src/main/java/br/com/valdircezar/wishlist/controllers/WishlistController.java
@@ -5,14 +5,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "v1/wishlists")
 @Tag(name = "Wishlist", description = "Controller responsible for managing wishlists")
---
+++ src/main/java/br/com/valdircezar/wishlist/models/exceptions/StandardError.java
@@ -0,0 +1,16 @@
+package br.com.valdircezar.wishlist.models.exceptions;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+public class StandardError {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
